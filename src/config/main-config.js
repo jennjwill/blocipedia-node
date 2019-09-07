8288b72ddb41708c5fcfd7e3a41f667081ef84d1
Jenn J Will <jennijwill@gmail.com>
2019-09-07 11:12:25 -0700

    scaffolding and deployment checkpoint completed

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -1,5 +1,11 @@
 require("dotenv").config();
 
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+
 module.exports = {
-  init() {}
+  init(app, express) {
+    app.use(logger("dev"));
+    app.use(bodyParser.json());
+  }
 };
