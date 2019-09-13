93f55e4a8e8d7f0e7fd6e400f0b4b7346f12ad09
Jenn J Will <jennijwill@gmail.com>
2019-09-13 13:06:28 -0700

    added user view and controller, cannot GET users/sign_in

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -1,11 +1,15 @@
 require("dotenv").config();
-
 const logger = require("morgan");
 const bodyParser = require("body-parser");
+const path = require("path");
+const viewsFolder = path.join(__dirname, "..", "views");
 
 module.exports = {
   init(app, express) {
+    app.set("views", viewsFolder);
+    app.set("view engine", "ejs");
     app.use(logger("dev"));
     app.use(bodyParser.json());
+    app.use(express.static(path.join(__dirname, "..", "assets")));
   }
 };
