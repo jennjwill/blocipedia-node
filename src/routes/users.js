10522f9e7f14b2c37dfaf733fe0f86ba5ea8eebc
Jenn J Will <jennijwill@gmail.com>
2019-09-13 16:39:19 -0700

    fixed sign_in route, added sendgrid, modeled User, added some specs

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 
 //router.get("/users", userController.index);
-router.get("users/sign_up", userController.signUp);
+router.get("/users/sign_up", userController.signUp);
 
 module.exports = router;
