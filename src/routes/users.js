b2dac0076f39ca15b8f7438ec726ebc093711c0f
Jenn J Will <jennijwill@gmail.com>
2019-09-19 11:35:46 -0700

    upgrading an account completed

--- src/routes/users.js
+++ src/routes/users.js
@@ -18,6 +18,8 @@ router.get("/users/upgrade", userController.upgrade);
 
 router.post("/users/:id/upgrade", userController.payment);
 
+router.get("/users/downgrade", userController.downgrade);
+
 router.post("/users/:id/downgrade", userController.downgrade);
 
 module.exports = router;
